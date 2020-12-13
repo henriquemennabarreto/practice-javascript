912e37aae85fd5057a9abbe4c3595e184cda9c58
Henrique Menna Barreto <henrique.araujo@kinghost.com.br>
2020-12-13 17:23:23 -0300

    Added method to search by the index

--- SequentialList.js
+++ SequentialList.js
@@ -4,4 +4,9 @@ class SequentialList {
     this.last = -1;
     this.list.length = size;
   }
+
+  getElement(index) {
+    let element = (index < 0 || index > this.last) ? false : this.list[index];
+    return element;
+  }
 }
