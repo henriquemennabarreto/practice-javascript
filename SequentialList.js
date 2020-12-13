9c760b0d6d7fd2ee641876b3173e6887d2fda0a1
Henrique Menna Barreto <henrique.araujo@kinghost.com.br>
2020-12-13 17:24:09 -0300

    Add and AddByIndex

--- SequentialList.js
+++ SequentialList.js
@@ -9,4 +9,19 @@ class SequentialList {
     let element = (index < 0 || index > this.last) ? false : this.list[index];
     return element;
   }
+
+  add(value) {
+    if (this.isFull() === true) return false;
+    this.last++;
+    this.list[this.last] = value;
+    return true;
+  }
+
+  addByIndex(value, index) {
+    for (let i = this.last + 1; i > index; i--) {
+      this.list[i] = this.list[i - 1];
+    }
+    this.last++;
+    this.list[index] = value;
+  }
 }
